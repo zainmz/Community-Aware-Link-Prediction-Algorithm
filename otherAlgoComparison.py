89a44d587970191c54b4cf59ebe55c9fce0098ee
zainmz <zainmzameer@gmail.com>
2023-10-07 23:16:17 +0530

    Update otherAlgoComparison.py

--- otherAlgoComparison.py
+++ otherAlgoComparison.py
@@ -20,15 +20,15 @@ sampled_non_edges = random.sample(non_edges, int(sample_size * len(non_edges)))
 
 # Check if pre-processed features exist, if yes, load them
 if os.path.exists('features.pkl'):
-    print("Loading features from file...")
+    print("Loading file...")
     with open('features.pkl', 'rb') as file:
         data = pickle.load(file)
         X = data['features']
         y = data['labels']
 
 # Split the data into training and testing sets
-print("Splitting data into train and test sets...")
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+print("Training and testing data splitting...")
+Z_train, Z_test, a_train, a_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Check if a pre-trained model exists, if yes, load it
 if os.path.exists('model.pkl'):
@@ -38,16 +38,16 @@ if os.path.exists('model.pkl'):
 
 # Use the classifier to predict the links
 print("Predicting with CALP algorithm...")
-y_pred_calp = classifier.predict(X_test)
+a_pred_calp = classifier.predict(Z_test)
 
 # Prepare data for other algorithms
 print("Extract data for other algorithms to run...")
 df_X = pd.DataFrame(X, columns=['feature1', 'feature2', 'feature3', 'feature4'])
-df_X_test = pd.DataFrame(X_test, columns=['feature1', 'feature2', 'feature3', 'feature4'])
-common_rows = df_X.merge(df_X_test, how='inner', left_index=True, right_index=True)
-X_test_indices = common_rows.index.tolist()
+df_Z_test = pd.DataFrame(Z_test, columns=['feature1', 'feature2', 'feature3', 'feature4'])
+common_rows = df_X.merge(df_Z_test, how='inner', left_index=True, right_index=True)
+Z_test_indices = common_rows.index.tolist()
 all_pairs = edges + non_edges
-X_test_pairs = [all_pairs[i] for i in X_test_indices]
+Z_test_pairs = [all_pairs[i] for i in Z_test_indices]
 
 # Helper function to predict based on a threshold
 def predict_with_threshold(scores, threshold=0.5):
@@ -59,25 +59,25 @@ def common_neighbors_score(G, u, v):
 
 # Predict using other link prediction algorithms
 print("Predicting with Common Neighbors...")
-y_pred_common_neighbors = predict_with_threshold([common_neighbors_score(G, u, v) for u, v in X_test_pairs])
+a_pred_common_neighbors = predict_with_threshold([common_neighbors_score(G, u, v) for u, v in Z_test_pairs])
 print("Predicting with Jaccard Coefficient...")
-y_pred_jaccard = predict_with_threshold([p for u, v, p in nx.jaccard_coefficient(G, ebunch=X_test_pairs)])
+a_pred_jaccard = predict_with_threshold([p for u, v, p in nx.jaccard_coefficient(G, ebunch=Z_test_pairs)])
 print("Predicting with Adamic-Adar Index...")
-y_pred_adamic_adar = predict_with_threshold([p for u, v, p in nx.adamic_adar_index(G, ebunch=X_test_pairs)])
+a_pred_adamic_adar = predict_with_threshold([p for u, v, p in nx.adamic_adar_index(G, ebunch=Z_test_pairs)])
 print("Predicting with Preferential Attachment...")
-y_pred_preferential_attachment = predict_with_threshold([p for u, v, p in nx.preferential_attachment(G, ebunch=X_test_pairs)])
+a_pred_preferential_attachment = predict_with_threshold([p for u, v, p in nx.preferential_attachment(G, ebunch=Z_test_pairs)])
 
 # Evaluate and print the performance of each algorithm
 print("Evaluating all predictions...")
 algorithms = ['CALP', 'Common Neighbors', 'Jaccard', 'Adamic-Adar', 'Preferential Attachment']
-predictions = [y_pred_calp, y_pred_common_neighbors, y_pred_jaccard, y_pred_adamic_adar, y_pred_preferential_attachment]
+predictions = [a_pred_calp, a_pred_common_neighbors, a_pred_jaccard, a_pred_adamic_adar, a_pred_preferential_attachment]
 
-for algo, y_pred in zip(algorithms, predictions):
+for algo, a_pred in zip(algorithms, predictions):
     print(f"Evaluation for {algo}:")
-    print(f"Accuracy: {accuracy_score(y_test, y_pred)}")
-    print(f"Precision: {precision_score(y_test, y_pred)}")
-    print(f"Recall: {recall_score(y_test, y_pred)}")
-    print(f"F1 Score: {f1_score(y_test, y_pred)}")
+    print(f"Score_Accuracy: {accuracy_score(a_test, a_pred)}")
+    print(f"Score_Precision: {precision_score(a_test, a_pred)}")
+    print(f"Score_Recall: {recall_score(a_test, a_pred)}")
+    print(f"Score_F1: {f1_score(a_test, a_pred)}")
     print("-" * 50)
 
 print("Done!")
